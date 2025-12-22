aa31ef8062ff5bac7bcc2542479db404aa0bb333
my112233 <yanghyeonu738@gmail.com>
2025-12-22 22:11:29 +0900

    Feat : Ability

---
+++ GGGM_13/Assets/Members/Hyenwoo_F/02_Script/Ability/Heal.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Heal", menuName = "Scriptable Objects/Heal")]
+public class Heal : CardAbilitySO
+{
+    [SerializeField] private int healValue;
+    public override void Excute()
+    {
+        YHWGameManager.Instance.Player.GetComponent<PlayerHP>().GetHeal(healValue);
+    }
+}
