d4d24621accf4eb24b2f67f42f50e5105b9f1c3d
my112233 <yanghyeonu738@gmail.com>
2025-12-23 18:55:11 +0900

    Fix : Reflection Txt

--- GGGM_13/Assets/Members/Hyenwoo_F/02_Script/ReflectionEffectManager.cs
+++ GGGM_13/Assets/Members/Hyenwoo_F/02_Script/ReflectionEffectManager.cs
@@ -1,16 +1,46 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ReflectionEffectManager : MonoBehaviour
 {
     [SerializeField] private GameObject reflectionTxt;
+    private List<GameObject> reflectionTxtPool;
 
     private void Start()
     {
-        reflectionTxt.SetActive(false);
+        ReflectionTxtPool();
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            ShowEffect(25);
+        }
     }
 
     public void ShowEffect(int persent)
     {
-        reflectionTxt.GetComponent<ReflectionTxtAnim>().Show(persent);
+        foreach (var item in reflectionTxtPool)
+        {
+            if (!item.activeSelf)
+            {
+                item.transform.position = transform.position;
+                item.GetComponent<ReflectionTxtAnim>().Show(persent);
+                break;
+            }
+        }
+    }
+
+    private void ReflectionTxtPool()
+    {
+        reflectionTxtPool = new List<GameObject>();
+        for (int i = 0; i < 4; i++)
+        {
+            reflectionTxtPool.Add(Instantiate(reflectionTxt, gameObject.transform));
+            reflectionTxtPool[i].SetActive(false);
+        }
     }
 }
