d075ee5c7031f971bd9099bd664b1e86520f4297
pododang1120 <hwk9114@naver.com>
2025-12-23 10:40:09 +0900

    spawnEnemy

--- GGGM_13/Assets/Members/Hanul_F/02_Script/Enemy/SpawnEnemy.cs
+++ GGGM_13/Assets/Members/Hanul_F/02_Script/Enemy/SpawnEnemy.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
@@ -14,21 +16,70 @@ public class SpawnEnemy : MonoBehaviour
     void Start()
     {
         StageManager.Instance._onNextStage += SettingStage;
+        SettingStage(1);
     }
 
     public void SettingStage(int stage)
     {
+        Debug.Log("스테이지 증가함");
         currentStage = stage;
         if(currentStage % 2 == 0)
         {
             MaxEenmy += 2;
         }
-        EnemySpwan();
+        EnemyCount = 0;
+        StartCoroutine(EnemySpwan());
     }
 
-    private void EnemySpwan()
+    private IEnumerator EnemySpwan()
     {
-        
+        int oneCount;
+        GameObject enemy;
+        if (MaxEenmy - EnemyCount >= 5)
+        {
+            oneCount = 5;
+        }
+        else if (MaxEenmy - EnemyCount == 0)
+        {
+            oneCount = MaxEenmy;
+        }
+        else
+        {
+            oneCount = MaxEenmy - EnemyCount;
+        }
+        Debug.Log(EnemyCount);
+        Debug.Log(MaxEenmy);
+        Debug.Log(oneCount);
+
+
+        for (int i =  0;  i < oneCount;i++ )
+        {
+            EnemyCount++;
+            if (currentStage <= 4)
+            {
+                enemy =  Instantiate(enemys[0],spawnPoint.position,Quaternion.identity);
+                enemy.GetComponent<EnemyMovement>().Setting(center);
+                yield return new WaitForSeconds(2f);
+            }
+            else if (currentStage <= 8)
+            {
+                enemy = Instantiate(enemys[1],spawnPoint.position,Quaternion.identity);
+                enemy.GetComponent<EnemyMovement>().Setting(center);
+                yield return new WaitForSeconds(2f);
+            }
+            else
+            {
+                enemy =  Instantiate(enemys[UnityEngine.Random.Range(0,enemys.Count)]);
+                enemy.GetComponent<EnemyMovement>().Setting(center);
+                yield return new WaitForSeconds(2f);
+            }
+        }
+
+        if (EnemyCount < MaxEenmy)
+        {
+            yield return new WaitForSeconds(1f);
+            EnemySpwan();
+        }
     }
 
     void OnDisable()
