525413cbfb79f891a9afb83060267760a710a99f
pododang1120 <hwk9114@naver.com>
2025-12-23 20:13:26 +0900

    animal

--- GGGM_13/Assets/Members/Hanul_F/02_Script/Enemy/Deer.cs
+++ GGGM_13/Assets/Members/Hanul_F/02_Script/Enemy/Deer.cs
@@ -1,9 +1,20 @@
 using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 
 public class Deer : Enemy
 {
     [SerializeField] private GameObject DamgeParticle;
+
+    private readonly int isDead = Animator.StringToHash("IsDead");
+
+    private SpriteRenderer sr;
+    [SerializeField] private float duration = 0.3f;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
     protected override void OnDamge()
     {
         GameObject particle = Instantiate(DamgeParticle,gameObject.transform.position, Quaternion.identity);
@@ -12,12 +23,17 @@ public class Deer : Enemy
 
     protected override void OnDeath()
     {
-        base.enemyMovement.Speed += 2.25f;
+        base.enemyMovement.Speed += 1.6f;
     }
 
     protected override IEnumerator Remove()
     {
-        yield return new WaitForSeconds(2f);
-        Destroy(gameObject);
+        yield return new WaitForSeconds(1f);
+        Play();
+    }
+
+    public void Play()
+    {
+        sr.DOFade(0f, duration).OnComplete(() => Destroy(gameObject));
     }
 }
